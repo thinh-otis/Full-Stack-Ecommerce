c08b4462c940a068bc32ac39a99aa5fb05209b74
Thịnh Otis <aephone.gsm@gmail.com>
2024-10-26 22:28:06 +0700

    update currency for  frontend

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,12 +1,12 @@
-const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
+const displayINRCurrency = (num) =>{
+    const formatter = new Intl.NumberFormat('vi-VI',{
         style : "currency",
-        currency : 'INR',
-        minimumFractionDigits : 2
+        currency : 'VND',
+        minimumFractionDigits : 0   // nếu tính theo ngoại tệ thì thêm 2 số 0 phía sau
     })
-
     return formatter.format(num)
 
 }
 
 export default displayINRCurrency
+
