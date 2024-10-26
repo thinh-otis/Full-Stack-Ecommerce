4741db1d932184984d0082c08fd03e34e09c140c
Thịnh Otis <aephone.gsm@gmail.com>
2024-10-26 21:42:03 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,75 @@
+import React, { useCallback, useEffect } from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import SummaryApi from './common';
+import Context from './context';
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from './store/userSlice';
+import TrafficDataComponent from './components/TrafficDataComponent';
+import ScrollToTopButton from './components/ScrollToTopButton';
+
+
+function App() {
+  const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
+
+  const fetchUserDetails = useCallback(async()=>{
+      const dataResponse = await fetch(SummaryApi.current_user.url,{
+        method : SummaryApi.current_user.method,
+        credentials : 'include'
+      })
+
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        dispatch(setUserDetails(dataApi.data))
+      }
+  },[dispatch])
+
+  const fetchUserAddToCart = async()=>{
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    setCartProductCount(dataApi?.data?.count)
+  }
+
+  useEffect(()=>{
+    /**user Details */
+    fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
+
+  },[fetchUserDetails])
+  return (
+    <>
+      <Context.Provider value={{
+        fetchUserDetails, // user detail fetch 
+        cartProductCount, // current user add to cart product count,
+        fetchUserAddToCart
+      }}>
+        <ToastContainer
+          position='top-center'
+        />
+
+        <Header />
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet />
+        </main>
+        <TrafficDataComponent />
+        <ScrollToTopButton />
+        <Footer />
+      </Context.Provider>
+    </>
+  );
+}
+
+export default App;
