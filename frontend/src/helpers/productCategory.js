ca6d1e10022a675756fc3a9528886aad79b9e37b
Thịnh Otis <aephone.gsm@gmail.com>
2024-10-26 22:53:04 +0700

    update productCategory for  frontend

--- frontend/src/helpers/productCategory.js
+++ frontend/src/helpers/productCategory.js
@@ -1,17 +1,16 @@
 const productCategory = [
-    { id : 1, label : "Airpodes", value : "airpodes"},
-    { id : 2, label : "Camera", value : "camera"},
-    { id : 3, label : "Earphones", value : "earphones"},
-    { id : 4, label : "Mobiles", value : "mobiles"},
-    { id : 5, label : "Mouse", value : "Mouse"},
-    { id : 6, label : "Printers", value : "printers"},
-    { id : 7, label : "Processor", value : "processor"},
-    { id : 8, label : "Refrigerator", value : "refrigerator"},
-    { id : 9, label : "Speakers", value : "speakers"},
-    { id : 10, label : "Trimmers", value : "trimmers"},
-    { id : 11, label : "Televisions", value : "televisions"},
-    { id : 12, label : "Watches", value : "watches"},
+    { id : 1, label : "Tai nghe Bluetooth", value : "airpod" },
+    { id : 2, label : "Máy ảnh", value : "camera" },
+    { id : 3, label : "Tai nghe dây", value : "earphone" },
+    { id : 4, label : "Điện thoại", value : "mobile" },
+    { id : 5, label : "Chuột máy tính", value : "mouse" },
+    { id : 6, label : "Máy in", value : "printer" },
+    { id : 7, label : "Vi xử lý", value : "processor" },
+    { id : 8, label : "Tủ lạnh", value : "refrigerator" },
+    { id : 9, label : "Loa Bluetooth", value : "speaker" },
+    { id : 10, label : "Máy cắt tóc", value : "trimmer" },
+    { id : 11, label : "Smart Tivi", value : "television" },
+    { id : 12, label : "Đồng hồ thông minh", value : "watch" },
 ]
-
 
 export default productCategory
